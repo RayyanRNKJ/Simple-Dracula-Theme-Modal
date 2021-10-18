2f46de229c7cba38a8958bb71dbd5c9a9e3129c3
Rayyan Rasheed <rayyanwebofficial@gmail.com>
2021-10-18 16:19:30 +0530

    Add Modal

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const openModalBtn = document.querySelector(".open-modal-btn");
+const closeModalBtn = document.querySelector(".close-modal-btn")
+const modalContainer = document.querySelector(".modal-container");
+const modal = document.querySelector(".modal");
+
+const openModal = () => {
+  modalContainer.classList.add("open-modal-container");
+  modal.classList.add("open-modal");
+};
+
+const closeModal = () => {
+  modalContainer.classList.remove("open-modal-container");
+  modal.classList.remove("open-modal");
+};
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
